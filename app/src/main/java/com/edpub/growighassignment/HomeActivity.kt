9e22eb908e5e2890e8af712a07a06e61ce58be31
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2023-07-06 22:02:26 +0530

    Home Activity Corrected

--- app/src/main/java/com/edpub/growighassignment/HomeActivity.kt
+++ app/src/main/java/com/edpub/growighassignment/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.edpub.growighassignment
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 
@@ -11,13 +12,13 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val bFeeds = findViewById<ConstraintLayout>(R.id.bFeeds)
+        val bFeeds = findViewById<TextView>(R.id.bFeeds)
         bFeeds.setOnClickListener {
             val intent = Intent(this, FeedActivity::class.java)
             startActivity(intent)
         }
 
-        val bUploadImageActivity = findViewById<ConstraintLayout>(R.id.bUpload)
+        val bUploadImageActivity = findViewById<TextView>(R.id.bUpload)
         bUploadImageActivity.setOnClickListener {
             val intent = Intent(this, UploadImageActivity::class.java)
             startActivity(intent)
