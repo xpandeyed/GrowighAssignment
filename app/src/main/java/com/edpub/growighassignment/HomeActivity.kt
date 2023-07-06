a01ca5f9a4a5906651f7c0a5bb67c4d0d58583ed
Lal Bihari Pandey <lalbiharipandeyg@gmail.com>
2023-07-06 14:23:25 +0530

    Home screen done for now

--- app/src/main/java/com/edpub/growighassignment/HomeActivity.kt
+++ app/src/main/java/com/edpub/growighassignment/HomeActivity.kt
@@ -4,19 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        val bFeeds = findViewById<Button>(R.id.bFeeds)
+        val bFeeds = findViewById<ConstraintLayout>(R.id.bFeeds)
         bFeeds.setOnClickListener {
             val intent = Intent(this, FeedActivity::class.java)
             startActivity(intent)
         }
 
-        val bUploadImageActivity = findViewById<Button>(R.id.bUpload)
+        val bUploadImageActivity = findViewById<ConstraintLayout>(R.id.bUpload)
         bUploadImageActivity.setOnClickListener {
             val intent = Intent(this, UploadImageActivity::class.java)
             startActivity(intent)
